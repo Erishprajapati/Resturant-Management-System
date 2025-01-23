cfcc52395d5eebe0f4f883a5dd8521f7247b58eb
scrumbler <irishmjn@gmail.com>
2025-01-23 16:23:29 +0545

    Model Correction

--- project/settings.py
+++ project/settings.py
@@ -23,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
